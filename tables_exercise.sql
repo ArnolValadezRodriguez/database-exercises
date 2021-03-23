bc6cb6c2f79ee295437726b44524554db8581ed4
Arnol Valadez Rodriguez <arnolvaladezr@gmail.com>
2021-03-23 16:58:05 -0500

    adding tables_exercise

---
+++ tables_exercise.sql
@@ -0,0 +1,24 @@
+3. Use the employees database
+    USE employees;
+
+4. List all the tables in the database
+    SHOW TABLES;
+
+5. Explore the employees table. What different data types are present on this table?
+    -DESCRIBE employees; 
+    -int(11), date, varchar(14), varchar(16), enum('M','F')
+
+6. Which table(s) do you think contain a numeric type column?
+
+
+7. Which table(s) do you think contain a string type column?
+
+
+8. Which table(s) do you think contain a date type column?
+
+
+9. What is the relationship between the employees and the departments tables?
+
+
+10. Show the SQL that created the dept_manager table.
+    
